1b6085b0cbd0eb76307ff62448bbfae3bbb4701f
Gmauder <gem00018@mix.wvu.edu>
2023-02-15 21:20:22 -0500

    added 4 components, not properly lined up yet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBArComponent } from './Navigation/nav-bar/nav-bar.component';
+import { OpinionCardComponent } from './opinion-card/opinion-card.component';
+import { EventCardComponent } from './event-card/event-card.component';
+import { NewsCardComponent } from './news-card/news-card.component';
+import { StreamComponent } from './stream/stream.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBArComponent
+    NavBArComponent,
+    OpinionCardComponent,
+    EventCardComponent,
+    NewsCardComponent,
+    StreamComponent
   ],
   imports: [
     BrowserModule,
